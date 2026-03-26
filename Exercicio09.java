901e783865145cc1de83cb7c3190014e72dbce85
senai-eliezer-sp <160336233+senai-eliezer-sp@users.noreply.github.com>
2026-03-26 19:39:12 -0300

    Update Exercicio09.java

--- Exercicio09.java
+++ Exercicio09.java
@@ -162,36 +162,6 @@ public class Exercicio06 {
     }
 }
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-
-package exercicio09;
-
-import java.util.Scanner;
-
-public class Exercicio09 {
-
-    public static void main(String[] args) {
-        
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.println("Digite um numero inteiro: ");
-        int numero = scanner.nextInt();
-        
-        System.out.println("\nTabuada do " + numero + ":\n");
-        for(int i = 1; i <= 10; i++){
-        
-        System.out.printf("%d x %2d = %3d%n", numero, i, numero * i);
-        
-        }
-            
-        
-        
-
-        
-    }
-    
-}
---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 public class Exercicio07 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -219,6 +189,84 @@ public class Exercicio07 {
     }
 }
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+package Exercicio08;
+
+import java.util.Scanner;
+
+public class Exercicio08 {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Digite um numero de 1 a 7: ");
+        int numero = sc.nextInt();
+        
+        switch(numero){
+            case 1:
+                System.out.println("Domingo");
+        break;
+            case 2:
+               System.out.println("Segunda-Feira");
+        break;
+            case 3:
+                System.out.println("Terça-Feira");
+        break;
+            case 4:
+                System.out.println("Quarta-Feira");
+        break;
+            case 5:
+                System.out.println("Quinta-Feira");
+        break;
+            case 6:
+                System.out.println("Sexta-Feira");
+        break;
+            case 7:
+                System.out.println("Sabado");
+        break;
+            default:
+            System.out.println("Data invalida!!");
+    }
+                
+               
+       
+    }
+
+    }
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+package exercicio09;
+
+import java.util.Scanner;
+
+public class Exercicio09 {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Digite um numero inteiro: ");
+		
+        int numero = scanner.nextInt();
+        
+        System.out.println("\nTabuada do " + numero + ":\n");
+		
+        for(int i = 1; i <= 10; i++){
+        
+        System.out.printf("%d x %2d = %3d%n", numero, i, numero * i);
+        
+        }
+            
+        
+        
+
+        
+    }
+    
+}
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 import java.util.Scanner;
 
 public class Exercicio10 {
