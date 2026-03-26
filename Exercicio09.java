950d36a1530638c72a4ad9a7c3b61986cc79fdbd
senai-eliezer-sp <160336233+senai-eliezer-sp@users.noreply.github.com>
2026-03-26 18:09:33 -0300

    Update Exercicio09.java

--- Exercicio09.java
+++ Exercicio09.java
@@ -54,5 +54,43 @@ public class Exercicio07 {
     }
 }
 
+import java.util.Scanner;
+
+public class Exercicio10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numero;
+        int soma = 0;
+        int contador = 0;
+
+        System.out.println("Digite números inteiros e para encerrar digite 0:");
+        
+        numero = scanner.nextInt();
+
+        while (numero != 0) {
+            soma += numero;
+            contador++;    
+
+            numero = scanner.nextInt();
+        }
+
+        System.out.println("\nResultado:");
+
+        System.out.println("Soma: " + soma);
+
+        System.out.println("Quantidade: " + contador);
+
+        if (contador > 0) {
+
+            double media = (double) soma / contador;
+
+            System.out.println("Média: " + media);
+        }
+	else {
+            System.out.println("Média: não é possível calcular (nenhum número válido).");
+        }
+    }
+}
 
 
