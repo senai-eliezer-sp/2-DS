7252922aadf6f57752efc2b942cf46b767b38ece
senai-eliezer-sp <160336233+senai-eliezer-sp@users.noreply.github.com>
2026-03-26 18:05:57 -0300

    Add Exercicio07 class for triangle type evaluation

--- Exercicio09.java
+++ Exercicio09.java
@@ -26,3 +26,33 @@ public class Exercicio09 {
     }
     
 }
+
+public class Exercicio07 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o lado A: ");
+        double a = sc.nextDouble();
+
+        System.out.print("Digite o lado B: ");
+        double b = sc.nextDouble();
+
+        System.out.print("Digite o lado C: ");
+        double c = sc.nextDouble();
+
+        if (a < b + c && b < a + c && c < a + b) {
+            if (a == b && b == c) {
+                System.out.println("Triângulo Equilátero");
+            } else if (a == b || a == c || b == c) {
+                System.out.println("Triângulo Isósceles");
+            } else {
+                System.out.println("Triângulo Escaleno");
+            }
+        } else {
+            System.out.println("Os valores não formam um triângulo válido.");
+        }
+    }
+}
+
+
+
